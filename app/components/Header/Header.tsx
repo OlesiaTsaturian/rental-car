b8243d433b129858750edd1dcf99adc07acf8e28
Olesia Tsaturian <olesiases@gmail.com>
2026-02-03 20:04:57 +0200

    add header

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+export default function Header() {
+  return (
+    <nav>
+      <div>
+        <div>
+          <a href="">
+            <svg>
+              <use></use>
+            </svg>
+          </a>
+        </div>
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Catalog</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
